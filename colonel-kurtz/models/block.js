007775397c18701a0a7e3b54d21250e9666c2b2e
Lawson Kurtz <lawson.kurtz@viget.com>
2014-11-20 12:45:40 -0700

    Add todo message to blocklist creation test code

--- colonel-kurtz/models/block.js
+++ colonel-kurtz/models/block.js
@@ -34,6 +34,8 @@ class Block {
   }
 
   childBlockList() {
+    // To simulate Blocks with nested block lists, add a block list to every block.
+    // TODO: remove once block types are implemented
     BlockListActions.create({ blockId: this.id })
 
     return BlockListStore.findByBlockId(this.id)
