cd415ee675ccc8a43ca893d79714e8663b225cb8
Nate Hunzaker <nate.hunzaker@gmail.com>
2014-12-16 15:13:55 -0500

    [change] Insert test to check for special types of insertion

--- colonel-kurtz/models/__tests__/block_list-test.js
+++ colonel-kurtz/models/__tests__/block_list-test.js
@@ -15,10 +15,10 @@ describe('Models - Block List', function() {
   it ('can insert a block at a given index', function() {
     var b = new BlockList({})
 
-    b.insertBlock(0, 0)
-    b.insertBlock(1, 1)
+    b.insertBlock(0, 1)
+    b.insertBlock(1, 0)
 
-    expect(b.all()).toEqual([ 0, 1])
+    expect(b.all()).toEqual([ 1, 0])
   })
 
   it ('can remove a block', function() {
