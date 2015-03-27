6d86d40227236f217e1cf1d9fc7b2ddb024a36a9
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-03-27 13:34:29 -0400

    [add] Colonel Kurtz binary

--- bin/colonel_kurtz.js
+++ bin/colonel_kurtz.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
+'use strict';
 
+var raw     = process.argv[2];
 var migrate = require('../lib/migrate')
-console.log('hey')
+var json;
+
+if (!raw.trim()) {
+  process.exit(1);
+}
+
+json = JSON.parse(raw)
+
+console.log(JSON.stringify(migrate(json)))
