e50802fa7bca01db6572524df3071a4edf2ac877
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-04-15 11:52:47 -0400

    [add] Focus trap for menu management

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,16 @@
+import FocusTrap  from 'react-focus-trap'
 import React      from 'react'
-import classNames from 'classnames'
 
 export default React.createClass({
 
-  getDefaultProps() {
-    return { open: false }
-  },
-
   render() {
-    let className = classNames('col-menu', {
-      'col-menu-open' : this.props.open
-    })
-
     return (
-      <nav role="navigation" className={ className }>
-        <div className="col-menu-backdrop" aria-hidden="true" />
-        { this.props.children }
-      </nav>
+      <FocusTrap onExit={ this.props.onExit }>
+        <nav role="navigation" className="col-menu">
+          <div className="col-menu-backdrop" aria-hidden="true" />
+          { this.props.children }
+        </nav>
+      </FocusTrap>
     )
   }
 
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -22,23 +22,33 @@ export default React.createClass({
     this.setState({ open: false })
   },
 
-  render() {
+  getMenu() {
     let { app, block } = this.props
 
     let siblings = app.pull('blocks', siblingsOf, block)
     let isFirst  = siblings[0] === block
     let isLast   = siblings[siblings.length - 1] === block
 
+    return this.state.open ? (
+      <Menu onExit={ this._onExit }>
+        <Item ref="moveUp"   label="Move Up"   onClick={ app.prepare(shift, block.id, -1) } hide={ isFirst } />
+        <Item ref="moveDown" label="Move Down" onClick={ app.prepare(shift, block.id, 1) }  hide={ isLast } />
+        <Item ref="destroy"  label="Remove"    onClick={ app.prepare(destroy, block.id) } />
+      </Menu>
+    ) : null;
+  },
+
+  render() {
     return (
       <div className="col-toolbar">
         <Handle ref="handle" onClick={ this._onHandleClick }/>
-        <Menu open={ this.state.open }>
-          <Item ref="moveUp"   label="Move Up"   onClick={ app.prepare(shift, block.id, -1) } hide={ isFirst } />
-          <Item ref="moveDown" label="Move Down" onClick={ app.prepare(shift, block.id, 1) }  hide={ isLast } />
-          <Item ref="destroy"  label="Remove"    onClick={ app.prepare(destroy, block.id) } />
-        </Menu>
+        { this.getMenu() }
       </div>
     )
+  },
+
+  _onExit() {
+    this.setState({ open: false })
   },
 
   _onHandleClick(e) {
