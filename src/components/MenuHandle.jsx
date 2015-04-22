644980c38ecbbd1450775257c5a3cd0f81895089
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-04-22 09:42:01 -0400

    [update] Improve focus state of handle

--- src/components/MenuHandle.jsx
+++ src/components/MenuHandle.jsx
@@ -1,5 +1,6 @@
 import React    from 'react'
 import menuIcon from 'icons/menu'
+import Btn      from './Button'
 
 export default React.createClass({
 
@@ -15,11 +16,11 @@ export default React.createClass({
     let { label, ...safe } = this.props
 
     return (
-      <button { ...safe }>
+      <Btn { ...safe }>
         <span className="col-hidden">{ label }</span>
         <span aria-hidden="true"
               dangerouslySetInnerHTML={{ __html: menuIcon }} />
-      </button>
+      </Btn>
     )
   }
 
