5af233a34c2e4d657183f72ffe568aba40218f44
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-06-08 16:48:34 -0400

    [update] Rename private blocks to childOnly

--- src/utils/__tests__/typesForBlock.test.js
+++ src/utils/__tests__/typesForBlock.test.js
@@ -4,11 +4,11 @@ let typesForBlock = require('../typesForBlock')
 describe('Utils - typesForBlock', function() {
 
   describe('when not given a block', function() {
-    let private = new BlockType({ id: 'private', private: true })
-    let social  = new BlockType({ id: 'social' })
+    let childOnly = new BlockType({ id: 'child-only', childOnly: true })
+    let normal    = new BlockType({ id: 'social' })
 
-    it ('filters out private blocks', function() {
-      typesForBlock([ private, social ]).should.eql([ social ])
+    it ('filters out childOnly blocks', function() {
+      typesForBlock([ childOnly, normal ]).should.eql([ normal ])
     })
   })
 
