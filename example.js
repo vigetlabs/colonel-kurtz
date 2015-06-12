f4c21057bea855541a01f5b943a4191983e5048c
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-06-12 11:25:21 -0400

    [add] Call it root

--- example.js
+++ example.js
@@ -22,7 +22,7 @@ let blockTypes = [
     id        : 'child-text',
     label     : 'Child Text',
     component : require('../addons/medium'),
-    childOnly : true
+    root : true
   },
   {
     id        : 'image',
