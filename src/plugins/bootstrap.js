c2e166a91ed6e1d6cf93875b97b25f3cc9d98330
Eli Fatsi <eli.fatsi@viget.com>
2015-11-09 14:58:37 -0700

    Only passing in blocks with matching blockTypes

--- src/plugins/bootstrap.js
+++ src/plugins/bootstrap.js
@@ -15,18 +15,27 @@ let parseElement = function (element) {
 
 module.exports = {
 
-  filter(blockTypes, acceptable) {
+  filterBlockTypes(blockTypes, acceptable) {
     if (!acceptable) return blockTypes
 
     return blockTypes.filter(type => acceptable.indexOf(type.id) > -1)
   },
 
+  filterBlocks(blocks, blockTypes) {
+    var types = blockTypes.map(blockType => blockType.id)
+
+    return blocks.filter(block => types.indexOf(block.type) > -1)
+  },
+
   register(app, { allow, maxChildren, blocks, blockTypes }, next) {
+    blockTypes = this.filterBlockTypes(blockTypes, allow)
+
     if (blocks instanceof HTMLElement) {
       blocks = parseElement(blocks)
+      blocks = filterBlocks(blocks, blockTypes)
     }
 
-    app.replace({ blocks, blockTypes: this.filter(blockTypes, allow) })
+    app.replace({ blocks, blockTypes })
 
     app.set('maxChildren', maxChildren)
 
