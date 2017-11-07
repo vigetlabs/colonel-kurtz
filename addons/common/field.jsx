858f61e59120859a7a029f172851390f412a3ac1
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-11-07 08:34:29 -0500

    Remove uid package
    The uid package is no longer maintained. We use this package to
    generate unique ids for the <Field /> component's labels.
    
    This commit inlines that code, which is only three lines of code.
    
    Fixes #131

--- addons/common/field.jsx
+++ addons/common/field.jsx
@@ -4,7 +4,11 @@
  */
 
 let React = require('react')
-let uid   = require('uid')
+
+function uid(len) {
+  len = len || 7;
+  return Math.random().toString(35).substr(2, len);
+}
 
 let Field = React.createClass({
 
