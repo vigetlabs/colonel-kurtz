5e53903a7afa324a1d76cfafe3642ce82ed64c3d
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-11-08 07:42:26 -0500

    Upgrade webpack and related dependencies
    This temporarily removes test coverage. We'll get this back when we
    replace Karma with Jest.

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,10 @@
 var Webpack        = require('webpack')
-var HappyPack      = require('happypack')
 var isIntegration  = process.env.CONTINUOUS_INTEGRATION === 'true'
-var noCoverage     = process.env.NO_COVERAGE === 'true'
 
 module.exports = function(config) {
 
   if (isIntegration) {
     console.log('Running in integration')
-  }
-
-  if (noCoverage) {
-    console.log('Test coverage has been disabled')
   }
 
   config.set({
@@ -32,17 +26,10 @@ module.exports = function(config) {
       './addons/**/__tests__/*.test.js*' : [ 'webpack', 'sourcemap' ]
     },
 
-    reporters: noCoverage ? [ 'mocha' ] : [ 'mocha', 'coverage' ],
+    reporters: ['mocha'],
 
     mochaReporter: {
       output: 'minimal'
-    },
-
-    coverageReporter: {
-      reporters: [
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcov', subdir: 'report-lcov' }
-      ]
     },
 
     webpack: {
@@ -52,13 +39,11 @@ module.exports = function(config) {
         new Webpack.ProvidePlugin({
           React     : 'react',
           TestUtils : 'react-addons-test-utils'
-        }),
-
-        new HappyPack({ id: 'js' })
+        })
       ],
 
       resolve: {
-        extensions: ['', '.js', '.scss', '.css']
+        extensions: ['.js', '.scss', '.css']
       },
 
       module: {
@@ -66,16 +51,9 @@ module.exports = function(config) {
           {
             test    : /\.js$/,
             exclude : /node_modules/,
-            loader  : 'babel?optional=runtime',
-            happy   : { id: 'js' }
+            loader  : 'babel-loader'
           }
-        ],
-        postLoaders: noCoverage ? [] : [{
-          test    : /\.js$/,
-          happy   : { id: 'js' },
-          exclude : /(__tests__|node_modules)/,
-          loader  : 'istanbul-instrumenter'
-        }]
+        ]
       }
     },
 
